2bb95577e3d4d8a9e0db0c8df8f8ce75a32dddac
Yuyang He <Yuyang He@YuyangHe.home>
2015-09-26 09:59:26 +0800

    Leader Election repository created.

---
+++ CS431_Leader_Election/src/com/pikachu/cs431/test/Test.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.pikachu.cs431.test;
+
+/**
+ * @author Yuyang He
+ * @date 9:58:42 AM, Sep 26, 2015
+ * @version 1.0
+ * @since
+ */
+public class Test
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world!");
+	}
+
+}
